2129b768210ba64383be9670f279bed737619d8a
natalia.chebanenko <natalia.chebanenko@icloud.com>
2023-02-05 16:48:49 +0100

    Create weather api interface
    Created interface to fetch weather data

---
+++ SimpleWeatherConsole/SimpleWeatherConsole/Services/IWeatherApi.cs
@@ -0,0 +1,13 @@
+﻿using Refit;
+using SimpleWeatherConsole.Models;
+
+namespace SimpleWeatherConsole.Services
+{
+    [Headers(new[] { "X-RapidAPI-Key: 614d671d11msh0cd547c11af8aa6p1c4701jsncb7ddcae4a00", "X-RapidAPI-Host: weatherbit-v1-mashape.p.rapidapi.com" })]
+    public interface IWeatherApi
+	{
+            [Get("/current")]
+            Task<WeatherDTO> GetWeatherByCity(float lat, float lon);
+    }
+}
+
